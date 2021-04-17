03129cffa81619445963b2423bbc66602b9a4e80
Ashar Siddiqui <67350076+Ashar-Siddiqui@users.noreply.github.com>
2021-04-17 09:12:06 -0700

    Add files via upload

---
+++ linearregressionaiml.py
@@ -0,0 +1,23 @@
+import pandas as pd
+dataset = pd.read_csv('https://raw.githubusercontent.com/SiP-AI-ML/LessonMaterials/master/Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:,1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+import matplotlib.pyplot as plt
+# plot the actual data points of training set
+plt.scatter(X_train, y_train, color = 'red')
+# plot the regression line
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
